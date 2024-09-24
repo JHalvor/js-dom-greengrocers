e2788a7b627a6b10c598b35a6c05e84baeb848c4
Jonas <jonashalvorsen2013@live.no>
2024-09-24 10:34:14 +0200

    Core-Extension1-complete

--- index.js
+++ index.js
@@ -3,55 +3,66 @@ const state = {
     {
       id: "001-beetroot",
       name: "beetroot",
-      price: 0.35
+      price: 0.35,
+      type: "vegetable"
     },
     {
       id: "002-carrot",
       name: "carrot",
-      price: 0.35
+      price: 0.35,
+      type: "vegetable"
     },
     {
       id: "003-apple",
       name: "apple",
-      price: 0.35
+      price: 0.35,
+      type: "fruit"
     },
     {
       id: "004-apricot",
       name: "apricot",
-      price: 0.35
+      price: 0.35,
+      type: "fruit"
     },
     {
       id: "005-avocado",
       name: "avocado",
-      price: 0.35
+      price: 0.35,
+      type: "fruit"
     },
     {
       id: "006-bananas",
       name: "bananas",
-      price: 0.35
+      price: 0.35,
+      type: "fruit"
     },
     {
       id: "007-bell-pepper",
       name: "bell pepper",
-      price: 0.35
+      price: 0.35,
+      type: "vegetable"
     },
     {
       id: "008-berry",
       name: "berry",
-      price: 0.35
+      price: 0.35,
+      type: "fruit"
     },
     {
       id: "009-blueberry",
       name: "blueberry",
-      price: 0.35
+      price: 0.35,
+      type: "fruit"
     },
     {
       id: "010-eggplant",
       name: "eggplant",
-      price: 0.35
+      price: 0.35,
+      type: "vegetable"
     }
   ],
-  cart: []
+  cart: [],
+  selectedFilteredType: "None"
 };
 
 function render() {}
@@ -59,12 +70,14 @@ render();
 
 const storeList = document.querySelector(".store--item-list");
 const cartList = document.querySelector(".cart--item-list");
+const totalNumber = document.querySelector(".total-number");
+const filters = document.querySelector(".btn-group");
 
 function handleItemClick(storeItem) {
   //added item clicked to cart array?
-  const itemExists = state.cart.find((item) => item.id == storeItem.id)
-  if (itemExists) {
-    itemExists.quantity++
+  const cartItemExists = state.cart.find((item) => item.id == storeItem.id)
+  if (cartItemExists) {
+    cartItemExists.quantity++
   } else {
     const cartItem = { ...storeItem, quantity: 1 }
     state.cart.push(cartItem)
@@ -80,26 +93,39 @@ function handleCartItemClick(cartItem, increaseItemQuantity) {
   } else {
     cartItem.quantity--
     if (cartItem.quantity <= 0) {
-      state.cart.pop((item) => item.id === cartItem.id)
+      const cartItemIndex = state.cart.indexOf(cartItem)
+      state.cart.splice(cartItemIndex, 1)
     }
   }
 
   renderCart();
 }
 
+function handleFilterClick(type) {
+  state.selectedFilteredType = type
+  renderItems();
+}
+
 function renderItems() {
+  storeList.innerHTML = ``
+  console.log(state.selectedFilteredType)
   state.items.map((item) => {
-    let li = renderItem(item);
-    storeList.appendChild(li);
+    if (state.selectedFilteredType === "None" || state.selectedFilteredType === item.type) {
+      let li = renderItem(item);
+      storeList.appendChild(li);
+    }
   });
+  renderTotal();
 }
 
 function renderCart() {
+  cartList.innerHTML = ``
   //iterate through state.cart.map  and update the cart view
   state.cart.map((item) => {
     let li = renderCartItem(item);
     cartList.appendChild(li);
   });
+  renderTotal();
 }
 
 function renderCartItem(cartItem) {
@@ -111,7 +137,7 @@ function renderCartItem(cartItem) {
   img.setAttribute("alt", cartItem.name);
 
   const p = document.createElement("p");
-  p.innerText(cartItem.name)
+  p.textContent = cartItem.name
 
   const buttonRemove = document.createElement("button");
   buttonRemove.setAttribute("class", "quantity-btn remove-btn center")
@@ -121,7 +147,7 @@ function renderCartItem(cartItem) {
 
   const span = document.createElement("span")
   span.setAttribute("class", "quantity-text center")
-  span.innerText(cartItem.quantity)
+  span.textContent = cartItem.quantity
 
   const buttonAdd = document.createElement("button");
   buttonAdd.setAttribute("class", "quantity-btn add-btn center")
@@ -159,4 +185,39 @@ function renderItem(storeItem) {
   return li;
 }
 
+function renderTotal() {
+  let total = 0
+  state.cart.forEach((item) => {
+    total += item.price * item.quantity
+  })
+  totalNumber.textContent = total.toFixed(2)
+}
+
+function renderFilterButtons() {
+  filters.innerHTML = ``
+  filters.textContent = "Filter"
+
+  let types = []
+  state.items.forEach((item) => {
+    types.push(item.type)
+  })
+  types = [...new Set(types)]
+
+  renderFilterButton("None")
+
+  types.forEach((type) => {
+    renderFilterButton(type)
+  })
+}
+
+function renderFilterButton(type) {
+  const button = document.createElement("button")
+  button.innerText = type;
+  button.addEventListener("click", () => {
+    handleFilterClick(type);
+  });
+  filters.appendChild(button)
+}
+
 renderItems();
+renderFilterButtons();
